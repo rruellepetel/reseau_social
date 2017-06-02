19797337cb193f1e3d499245a01ff6f34836d1a2
rruellepetel <ruellepetelromain@gmail.com>
2017-06-02 13:32:24 +0200

    fix tags

--- people/views.py
+++ people/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView
 from .models import Profile
@@ -42,3 +42,6 @@ class ProfileUpdate(UpdateView):
     model = Profile
     slug_field = "user__username"
     fields = ['address', 'zip_Code', 'city', 'website', 'contact_Email', 'avatar', 'skills', 'interests']
+
+    def get_success_url(self):
+        return reverse('profile-detail', kwargs={'slug' : self.object.user.username})
