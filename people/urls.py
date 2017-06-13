2a2f495d9f7eb8f497b0621f216cfa71b2967043
rruellepetel <ruellepetelromain@gmail.com>
2017-06-13 16:14:36 +0200

    fix leaflet.js and improve django-leaflet

--- people/urls.py
+++ people/urls.py
@@ -3,7 +3,7 @@ from .views import ProfileDetailView,ProfileListView,ProfileUpdate,ProfileMapVie
 
 urlpatterns = [
     url(r'^list$', ProfileListView.as_view(), name="profile-list"),
-    url(r'^map/$', ProfileMapView.as_view(), name="profile-map"),
+    url(r'^map$', ProfileMapView.as_view(), name="profile-map"),
     url(r'^(?P<slug>[\w-]+)/edit$', ProfileUpdate.as_view(), name="profile-update"),
     url(r'^(?P<slug>[\w-]+)', ProfileDetailView.as_view(), name="profile-detail"),
 ]
